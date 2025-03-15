e4df7d170a04613afd0d6602e235c724b2bab104
Atik Jawad <85114480+AtikJawad@users.noreply.github.com>
2025-03-15 23:00:33 +0600

    Create Pseudoternary .m

---
+++ Pseudoternary .m
@@ -0,0 +1,49 @@
+%Pseudoternary (Reverse of Bipolar AMI)
+
+%the “0” symbol alternates between +V and –V and
+%the “1” symbol is represented by zero voltage
+
+clc;
+close all;
+clear;
+
+b=input('Binary data: ','s')-'0';
+n=length(b);
+t=0:0.01:n;
+
+for i=1:n
+  bw(i*100:(i+1)*100)=b(i);
+end
+bw=bw(100:end);
+
+subplot(2,1,1)
+plot(t,bw, 'linewidth',2);
+axis([0 n 0 +1]);
+grid on;
+title('Binary wave')
+
+lastbit=1;
+for i= 1:n
+ if(b(i)==0)
+  x(i*100:(i+1)*100)=lastbit;
+  lastbit=-lastbit;
+ else
+  x(i*100:(i+1)*100)=0;
+ endif
+end
+
+x=x(100:end);
+
+subplot(2,1,2)
+plot(t,x, 'linewidth',2);
+axis([0 n -1 +1]);
+grid on;
+title('Pseudoternary')
+
+
+
+
+
+
+
+
